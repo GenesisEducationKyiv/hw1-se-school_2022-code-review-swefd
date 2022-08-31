7ff914636bc39312205a5960bced5cc89b219623
Oleksandr Chornous <swefday@gmail.com>
2022-08-31 15:01:18 +0200

    change return await

--- src/services/rate.service.js
+++ src/services/rate.service.js
@@ -1,13 +1,13 @@
-//const COIN_PAIR = require("../../config/config.json").app.coinPair
 const axios = require("axios");
 
 class RateService {
   async getRate(coinPair = "BTCUAH") {
-    return await axios
+    const response = await axios
       .get(`https://api.binance.com/api/v3/ticker/price?symbol=${coinPair}`)
       .then((res) => {
         return Math.round(res.data["price"]).toString();
       });
+    return response;
   }
 }
 
