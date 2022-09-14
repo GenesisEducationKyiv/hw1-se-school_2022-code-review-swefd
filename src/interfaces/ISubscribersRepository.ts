9a67acde3c886c12d74f10c1166ca830e20d209f
Oleksandr Chornous <swefday@gmail.com>
2022-09-14 21:09:54 +0200

    Fix repository interface

--- src/interfaces/ISubscribersRepository.ts
+++ src/interfaces/ISubscribersRepository.ts
@@ -1,7 +1,7 @@
 import { ISubscriberModel } from "../models/subscriber.model";
 
 interface ISubscribersRepository {
-  append(subscriber: ISubscriberModel): any;
+  append(subscriber: ISubscriberModel): Promise<void>;
   includesEmail(email: string): Promise<boolean>;
   getAllSubscribers(): ISubscriberModel[];
   getAllEmails(): string[];
